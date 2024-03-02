f9935895796cc7437933f4328392960c4234930c
shashwatanshul <shashwatanshul@gmail.com>
2024-03-02 22:28:57 +0530

    Portfolio
    Portfolio

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { arrow } from "../assets/icons";
+
+const InfoBox = ({ text, link, btnText }) => (
+  <div className="info-box">
+    <p className="font-medium sm:text-xl text-center">{text}</p>
+    <Link to={link} className="neo-brutalism-white neo-btn">
+      {btnText}
+      <img src={arrow} className="w-4 h-4 object-contain" />
+    </Link>
+  </div>
+);
+
+const renderContent = {
+  1: (
+    <>
+      {" "}
+      <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+        Hi, I am <span className="font-semibold">Shashwat</span>👋
+        <br />A Full Stack Web Developer💻
+        <br />
+        <br />
+        <span className="">
+          To Ride the Aeroplane✈️,
+          <br />
+          Press and Hold ➡️ Right Arrow Key
+          <br />
+          OR
+          <br />
+          Drag/Swipe the Screen with Mouse/Finger
+        </span>
+      </h1>
+    </>
+  ),
+  2: (
+    <InfoBox
+      text="Worked with many businesses and picked up many skills along the way"
+      link="/about"
+      btnText="Learn more"
+    />
+  ),
+  3: (
+    <InfoBox
+      text="Led multiple projects to success over the years. Curious about the impact?"
+      link="/projects"
+      btnText="Visit my portfolio"
+    />
+  ),
+  4: (
+    <InfoBox
+      text="Need a project done or looking for a developer? I'm just a few keystrokes away"
+      link="/contact"
+      btnText="Let's talk"
+    />
+  ),
+};
+
+const HomeInfo = ({ currentStage }) => {
+  return renderContent[currentStage] || null;
+};
+
+export default HomeInfo;
